ce009ad57e5e25c1f834be76a33a6dd6388b0a31
Sitnikov Arseniy <sitnikov.aiu@phystech.edu>
2020-12-16 14:23:46 +0300

    Задач Уже встречались

--- Voting.py
+++ Voting.py
@@ -1,3 +1,13 @@
 dic = {}
 for i in range(kolichestvostudentov):
     print("What is your favourite movie?")
+    a = input()
+    if a in dic:
+        dic[a] += 1
+    else:
+        dic[a] = 1
+
+list_d = list(dic.items())
+list_d.sort(key=lambda i: i[1], reverse= True)
+for i in range(len(list_d)):
+    print(list_d[0])
